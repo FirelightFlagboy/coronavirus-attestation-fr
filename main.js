86ee9932c19bdc7ba49def3953a792a9db5e5f91
FirelightFlagboy <firelight.flagboy@gmail.com>
2020-03-17 15:47:32 +0100

    setup for print

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ document.getElementById("current-date").value = new Date().toISOString().substri
 let canvas = document.getElementById("signature");
 let clearSignCanvas = document.getElementById("clear-signature");
 
-document.getElementById("print").addEventListener("click", () => window.print());
+document.getElementById("form").addEventListener("submit", (e) => {
+	e.preventDefault();
+	window.print();
+});
 
-clearSignCanvas.addEventListener("click", () => {
+clearSignCanvas.addEventListener("click", (e) => {
 	canvas.getContext("2d").clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
-})
+});
+
 canvasAddEvents(canvas);
