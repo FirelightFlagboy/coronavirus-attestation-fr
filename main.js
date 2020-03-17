a906d9fb24efcf3cbe527bd6b205ee290390d37d
FirelightFlagboy <firelight.flagboy@gmail.com>
2020-03-17 15:20:44 +0100

    add button for pingting

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 document.getElementById("current-date").value = new Date().toISOString().substring(0, 10);
 
-let signCanvas = document.getElementById("signature");
+let canvas = document.getElementById("signature");
+let clearSignCanvas = document.getElementById("clear-signature");
 
-canvasAddEvents(signCanvas);
+document.getElementById("print").addEventListener("click", () => window.print());
+
+clearSignCanvas.addEventListener("click", () => {
+	canvas.getContext("2d").clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
+})
+canvasAddEvents(canvas);
