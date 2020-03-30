45187c55fe9a3d1f0963ee754ba20dfa3b83fbd4
FirelightFlagboy <firelight.flagboy@gmail.com>
2020-03-30 11:54:54 +0200

    wip js part

--- main.js
+++ main.js
@@ -1,15 +1,88 @@
-document.getElementById("current-date").value = new Date().toISOString().substring(0, 10);
+let motif_detail = {
+	"": "",
+	"work": "Déplacements entre le domicile et le lieu d’exercice de l’activité professionnelle, lorsqu’ils sont indispensables à l’exercice d’activités ne pouvant être organisées sous forme de télétravail ou déplacements professionnels ne pouvant être différés.",
+	"basic": "Déplacements pour effectuer des achats de fournitures nécessaires à l’activité professionnelle et des achats de première nécessité dans des établissements dont les activités demeurent autorisées (liste sur gouvernement.fr).",
+	"health": "Consultations et soins ne pouvant être assurés à distance et ne pouvant être différés ; consultations et soins des patients atteints d'une affection de longue durée.",
+	"family": "Déplacements pour motif familial impérieux, pour l’assistance aux personnes vulnérables ou la garde d’enfants.",
+	"trip": "Déplacements brefs, dans la limite d'une heure quotidienne et dans un rayon maximal d'un kilomètre autour du domicile, liés soit à l'activité physique individuelle des personnes, à l'exclusion de toute pratique sportive collective et de toute proximité avec d'autres personnes, soit à la promenade avec les seules personnes regroupées dans un même domicile, soit aux besoins des animaux de compagnie.",
+	"judicial": "Convocation judiciaire ou administrative.",
+	"mig": "Participation à des missions d’intérêt général sur demande de l’autorité administrative.",
+}
 
+document.getElementById("current-date").value = new Date().toLocaleString('fr-FR')
+
+/**
+ * canvas part
+ */
 let canvas = document.getElementById("signature");
 let clearSignCanvas = document.getElementById("clear-signature");
-
-document.getElementById("form").addEventListener("submit", (e) => {
-	e.preventDefault();
-	window.print();
-});
 
 clearSignCanvas.addEventListener("click", (e) => {
 	canvas.getContext("2d").clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
 });
 
 canvasAddEvents(canvas);
+
+/**
+ * motif part
+ */
+let motifDetailText = document.getElementById("motif-detail");
+let motifSelector = document.getElementById('motif');
+
+motifSelector.addEventListener('change', (e) => {
+	let value = e.target.value;
+	motifDetailText.textContent = motif_detail[value];
+})
+
+/**
+ * check box part
+ */
+let checkSign = document.getElementById('include-signature');
+let signBox = document.getElementById('sign-box');
+let includeSignature = checkSign.checked;
+
+let checkDate = document.getElementById('include-date');
+let currentDateBox = document.getElementById('current-date-box');
+let includeCurrrentDate = checkDate.checked;
+
+/**
+ *
+ * @param {HTMLElement} element
+ * @param {boolean} display
+ */
+function updateElementDisplay(element, display) {
+	if (display)
+		element.style.display = "";
+	else
+		element.style.display = "none";
+}
+
+updateElementDisplay(signBox, includeSignature);
+updateElementDisplay(currentDateBox, includeCurrrentDate);
+
+checkSign.addEventListener('change', (e) => {
+	includeSignature = e.target.checked;
+	updateElementDisplay(signBox, includeSignature);
+});
+
+checkDate.addEventListener('change', (e) => {
+	includeCurrrentDate = e.target.checked;
+	updateElementDisplay(currentDateBox, includeCurrrentDate);
+});
+
+/**
+ * submit part
+ */
+document.getElementById("form").addEventListener("submit", (e) => {
+	e.preventDefault();
+	let name = document.getElementById('name').value;
+	let born = document.getElementById('born_date').value;
+	let address = document.getElementById('address').value;
+	let reason = motifSelector.value;
+	let make_at_town = document.getElementById('town').value;
+	let at = document.getElementById('current-date').value;
+	let signature_uri = canvas.toDataURL();
+
+	console.log(name, born, address, reason, make_at_town, at, signature_uri);
+
+});
