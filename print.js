9967fbdc2bef9982013b4cbcfae4327b0086d2f2
FirelightFlagboy <firelight.flagboy@gmail.com>
2020-03-31 13:59:44 +0200

    edit print.js to set all data on print window

--- print.js
+++ print.js
@@ -14,22 +14,33 @@ const sdata = atob(edata);
 const data = JSON.parse(sdata);
 
 let nameField = document.getElementById('name');
-let bornDataField = document.getElementById('born-date');
-let addressField = document.getElementById('address');
+let bornWhenField = document.getElementById('born-when');
+let bornWhereField = document.getElementById('born-where');
+let residingField = document.getElementById('residing');
 let motifField = document.getElementById('motif');
 let whereField = document.getElementById('where');
-let whenField = document.getElementById('when');
+let whenDateField = document.getElementById('when-date');
+let whenTimeField = document.getElementById('when-time');
 let signField = document.getElementById('signature');
 
 nameField.textContent = data.name;
-bornDataField.textContent = new Intl.DateTimeFormat('fr-FR', dateTimeOptions)
-	.format(new Date(data.born_date));
-addressField.textContent = data.address
+bornWhenField.textContent = new Date(data['born-when']).toLocaleDateString();
+bornWhereField.textContent = data['born-where']
+residingField.textContent = data.residing
 motifField.textContent = motifs[data.reason].long;
-whereField.textContent = data.where;
+whereField.textContent = data['created-where'];
 
-if (data.when)
-	whenField.textContent = data.when;
+if (data['created-when-date']) {
+	whenDateField.textContent = new Date(data['created-when-date']).toLocaleDateString();
+}
+
+if (data['created-when-time']) {
+	whenTimeField.textContent = new Date(data['created-when-time']).toLocaleTimeString();
+}
 
 if (data.signature)
 	signField.src = data.signature;
+
+window.onload = () => {
+	window.print();
+}
